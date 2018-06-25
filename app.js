f89ca2ea6046720252a04ae2cb433886689a8d99
jimmy51997 <abhishekkumar51997@gmail.com>
2018-06-25 14:52:14 +0530

    uploading image to aws for pnb form

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ const cookieParser = require('cookie-parser') ;
 const session = require('express-session') ;
 const flash = require('express-flash') ;
 const validator = require('express-validator') ;
+const aws = require('aws-sdk');
 
+aws.config.region = 'ap-south-1';
+const S3_BUCKET = process.env.S3_BUCKET;
 
 const middleware = [
   bodyParser.urlencoded({extended: false}),
@@ -24,7 +27,33 @@ const middleware = [
   }),
   flash()
 ] ;
+// responsible for generating and returning the signature with which the client-side JavaScript can upload the image
+app.get('/sign-s3', (req, res) => {
+  const s3 = new aws.S3();
+  const fileName = req.query['file-name'];
+  const fileType = req.query['file-type'];
+  const s3Params = {
+    Bucket: S3_BUCKET,
+    Key: fileName,
+    Expires: 60,
+    ContentType: fileType,
+    ACL: 'public-read'
+  };
 
+  s3.getSignedUrl('putObject', s3Params, (err, data) => {
+    if(err){
+      console.log(err);
+      return res.end();
+    }
+    const returnData = {
+      signedRequest: data,
+      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+    };
+    res.write(JSON.stringify(returnData));
+    res.end();
+  });
+});
+//till here
 app.use(middleware) ;
 app.use(express.static('public')) ;
 app.use(express.static('bower_components')) ;
